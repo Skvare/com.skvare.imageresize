e285ffb6ce024e7f8fd66f5b2265f62727768398
Sunil Pawar <sunil@skvare.com>
2022-06-03 14:12:24 +0530

    show 404 status for non existing image

--- CRM/Imageresize/Page/ImageFile.php
+++ CRM/Imageresize/Page/ImageFile.php
@@ -40,11 +40,11 @@ class CRM_Imageresize_Page_ImageFile extends CRM_Contact_Page_ImageFile {
         'image/' . (strtolower($fileExtension) == 'jpg' ? 'jpeg' : $fileExtension),
         $this->imageTtl
       );
-      CRM_Utils_System::civiExit();
     }
     else {
-      throw new CRM_Core_Exception(ts('Photo does not exist'));
+      header("HTTP/1.0 404 Not Found");
     }
+    CRM_Utils_System::civiExit();
   }
 
 }
